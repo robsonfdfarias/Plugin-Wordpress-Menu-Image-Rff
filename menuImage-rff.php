1446d3c101ad23f8127cf56d4c6e103b74943385
Robson Ferreira de Fairas <robsonfdfarias@gmail.com>
2024-07-22 12:43:49 -0300

    Adicionado o css, a opção de excluir e a opção de alterar o registro.

--- menuImage-rff.php
+++ menuImage-rff.php
@@ -4,7 +4,8 @@ Plugin Name: Menu Image RFF
 Plugin URI:  http://exemplo.com
 Description: Cria um menu com imagens
 Version:     1.0
-Author:      Robson Farias
+Author:      Robson Ferreira de Farias
+Email: robsonfdfarias@gmail.com
 Author URI:  http://infocomrobson.com.br
 License:     GPL2
 */
@@ -13,6 +14,18 @@ License:     GPL2
  if(!defined('WPINC')){
     die();
  }
+
+ //chama o css
+ define('MI_RFF_URL_CSS', plugins_url('css/', __FILE__));// Caminho absoluto para o diretório do plugin
+ /***
+  * Registrando o css (frontend)
+  */
+  function mi_rff_register_css(){
+    wp_enqueue_style('mi-rff-css', MI_RFF_URL_CSS.'mi-rff.css', null, time(), 'all');
+ }
+
+ add_action('admin_enqueue_scripts', 'mi_rff_register_css');
+
 
 register_activation_hook(__FILE__, 'menuImage_rff_install');
 register_deactivation_hook(__FILE__, 'menuImage_rff_uninstall');
@@ -78,7 +91,6 @@ function menuImage_rff_excluir_dados($id) {
     global $wpdb;
     // $table_name = $wpdb->prefix . 'meu_plugin_table';
     $table_name = $wpdb->prefix . 'menuImage_rff';
-
     $wpdb->delete(
         $table_name,
         array('id' => $id), // Condição para atualizar (WHERE id = $id)
@@ -130,31 +142,41 @@ function menuImage_rff_admin_page() {
             <input type="file" name="urlImg" id="urlImg" accept="image/*">
             <input type="text" name="urlLink" placeholder="Digite a url do link" value="">
             <input type="text" name="altText" placeholder="Digite o texto que deve aparecer ao passar o mouse por cima da imagem" value="">
-            <input type="submit" value="Enviar">
+            <input type="submit" class="mi-rff-bt-submit" id="Enviar" name="Enviar" value="Enviar">
         </form>
     </div>
     <?php
-    // if(isset($_POST['nome'])){
-    //     echo $_POST['nome'].'***************<br>';
-    // }
-    // if(!empty($_FILES['urlImg']['name'])){
-    //     echo $_FILES['urlImg']['name'].'++++++++++++++++++<br>';
-    //     print_r($_FILES['urlImg']);
-    // }
-    // if (isset($_POST['nome']) && isset($_POST['urlImg']) && isset($_POST['urlLink']) && isset($_POST['altText'])) {
-    if (isset($_POST['nome']) && !empty($_FILES['urlImg']['name']) && isset($_POST['urlLink']) && isset($_POST['altText'])) {
-        $nome = sanitize_text_field($_POST['nome']);
-        $urlImg = $_FILES['urlImg'];
-        $urlLink = sanitize_text_field($_POST['urlLink']);
-        $altText = sanitize_text_field($_POST['altText']);
-        // printf($urlImg);
-        $image = uploadImage($urlImg);
-        // echo '//-----------------------------------//<br>';
-        // echo $image;
-        // echo '<br>........................................';
-        // menuImage_rff_gravar_dados($nome, $urlImg, $urlLink, $altText);
-        menuImage_rff_gravar_dados($nome, $image, $urlLink, $altText);
-        echo '<div class="notice notice-success is-dismissible"><p>Dados gravados com sucesso!</p></div>';
+    if(isset($_POST['Editar']) && isset($_POST['id']) && isset($_POST['nome']) && isset($_POST['urlImg']) && isset($_POST['urlLink']) && isset($_POST['altText'])){
+        if($_POST['id']!='' && $_POST['nome']!='' && $_POST['urlImg']!='' && $_POST['urlLink']!='' && $_POST['altText']!=''){
+            menuImage_rff_editar_dados($_POST['id'], $_POST['nome'], $_POST['urlImg'], $_POST['urlLink'], $_POST['altText']);
+            echo '<div class="notice notice-success is-dismissible"><p>Dados alterados com sucesso!</p></div>';
+        }else{
+            echo '<div class="notice notice-failure is-dismissible"><p>Todos os campos precisam ser preenchidos!</p></div>';
+        }
+    }else if (isset($_POST['Enviar']) && isset($_POST['nome']) && !empty($_FILES['urlImg']['name']) && isset($_POST['urlLink']) && isset($_POST['altText'])) {
+        if($_POST['nome']!='' && $_POST['urlLink']!='' && $_POST['altText']!=''){
+            $nome = sanitize_text_field($_POST['nome']);
+            $urlImg = $_FILES['urlImg'];
+            $urlLink = sanitize_text_field($_POST['urlLink']);
+            $altText = sanitize_text_field($_POST['altText']);
+            // printf($urlImg);
+            $image = uploadImage($urlImg);
+            // echo '//-----------------------------------//<br>';
+            // echo $image;
+            // echo '<br>........................................';
+            // menuImage_rff_gravar_dados($nome, $urlImg, $urlLink, $altText);
+            menuImage_rff_gravar_dados($nome, $image, $urlLink, $altText);
+            echo '<div class="notice notice-success is-dismissible"><p>Dados gravados com sucesso!</p></div>';
+        }else{
+            echo '<div class="notice notice-failure is-dismissible"><p>Todos os campos precisam ser preenchidos!</p></div>';
+        }
+    }else if(isset($_POST['Excluir']) && isset($_POST['id'])){
+        if($_POST['id']!=''){
+            menuImage_rff_excluir_dados($_POST['id']);
+            echo '<div class="notice notice-success is-dismissible"><p>Registro excluído com sucesso!</p></div>';
+        }else{
+            echo '<div class="notice notice-failure is-dismissible"><p>Não foi possível excluir o registro!</p></div>';
+        }
     }
     //mostra os dados gravados
     $dados = menuImage_rff_recuperar_dados();
@@ -162,16 +184,19 @@ function menuImage_rff_admin_page() {
         // echo '<img src="'.$dados[0]->urlImg.'" width="100">';
         echo '<h2>Dados Gravados</h2>';
         echo '<table class="wp-list-table widefat fixed striped">';
-        echo '<thead><tr><th>ID</th><th>Nome</th><th>Url Image</th><th>Url Link</th><th>Texto alternativo</th></tr></thead>';
+        echo '<thead><tr><th>ID</th><th>Nome</th><th>Url Image</th><th>Url Link</th><th>Texto alternativo</th><th>Ações</th></tr></thead>';
         echo '<tbody>';
         foreach ($dados as $dado) {
             echo '<tr>';
-            echo '<td>' . esc_html($dado->id) . '</td>';
-            echo '<td>' . esc_html($dado->nome) . '</td>';
+            echo '<form method="post" action="" enctype="multipart/form-data">';
+            echo '<td><input type="hidden" value="'.esc_html($dado->id).'" name="id" id="id" />' . esc_html($dado->id) . '</td>';
+            echo '<td><input type="text" value="' . esc_html($dado->nome) . '" name="nome" id="nome" placeholder="Digite o nome" /></td>';
             // echo '<td>' . esc_html($dado->urlImg) . '</td>';
-            echo '<td>' . '<img src="'.$dado->urlImg.'" width="100">' . '</td>';
-            echo '<td>' . esc_html($dado->urlLink) . '</td>';
-            echo '<td>' . esc_html($dado->altText) . '</td>';
+            echo '<td>' . '<img src="'.$dado->urlImg.'" class="mi-rff-img-admin"><input type="hidden" name="urlImg" id="urlImg" value="'.$dado->urlImg.'" /></td>';
+            echo '<td><input type="text" value="' . esc_html($dado->urlLink) . '" name="urlLink" id="urlLink" placeholder="Digite a url do link" /></td>';
+            echo '<td><input type="text" value="' . esc_html($dado->altText) . '" name="altText" id="altText" placeholder="Digite o texto alternativo" /></td>';
+            echo '<td><input type="submit" class="mi-rff-bt-submit" id="Editar" name="Editar" value="Editar" /><input type="submit" class="mi-rff-bt-submit" id="Excluir" name="Excluir" value="Excluir" /></td>';
+            echo '</form>';
             echo '</tr>';
         }
         echo '</tbody>';
